8850054a793d90e274f42fb32496b6d1aaf8eadd
Rajeshmane515 <Rajeshmane515@gmail.com>
2026-06-19 19:32:51 +0530

    Made hero section responsive

--- src/components/home/heroVisual/HeroVisual.jsx
+++ src/components/home/heroVisual/HeroVisual.jsx
@@ -13,52 +13,76 @@ const HeroVisual = () => {
         flex
         items-center
         justify-center
-        min-h-[650px]
         w-full
+        min-h-[420px]
+        md:min-h-[550px]
+        lg:min-h-[650px]
         overflow-hidden
       "
     >
-      {/* Background Glows */}
-
+      {/* Background Glow */}
       <GlowOrb
         size={250}
         color="violet"
         opacity={10}
-        className="bottom-50 right-40"
+        className="bottom-20 md:bottom-40 lg:bottom-50 right-10 md:right-20 lg:right-40"
       />
 
-      {/* Dashboard Area */}
+      {/* Mobile Layout */}
+      <div
+        className="
+          flex
+          flex-col
+          items-center
+          gap-5
+          w-full
+          lg:hidden
+          relative
+          z-10
+        "
+      >
+        <ConversionRateCard />
+        <QualifiedLeadsCard />
+        <ActiveCampaignsCard />
+      </div>
 
-      <div className="relative z-10">
+      {/* Desktop Layout */}
+      <div
+        className="
+          hidden
+          lg:block
+          relative
+          z-10
+          w-full
+          h-[650px]
+        "
+      >
         {/* Qualified Leads */}
-
         <QualifiedLeadsCard
           className="
             absolute
-             -right-12
-            top-0
+            -right-12
+            top-24
             z-20
           "
         />
 
         {/* Conversion */}
-
         <ConversionRateCard
           className="
             absolute
-           left-20
-            -top-40
+            left-16
+            top-0
             z-20
           "
         />
 
         {/* Active Campaigns */}
-
         <ActiveCampaignsCard
           className="
             absolute
             -right-12
-            bottom-10
+            bottom-16
             z-20
           "
         />
